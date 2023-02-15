66b2a976e4769f8814a6339fc046848d64b80759
linyuan <673609957@qq.com>
2023-02-15 17:24:02 +0800

    chore: golang

--- src/demo11.go
+++ src/demo11.go
@@ -6,6 +6,20 @@ func swap(x string, y string) (string, string) {
 	return y, x
 }
 
+func ptrSwap(x *int, y *int) {
+	println(*x) // 获取到这个指针指向的值
+	temp := *x  // 保存这个值
+	*x = *y     // 将y值赋值给x这个变量
+	*y = temp   // 将x值赋值给y这个变量
+}
+func ptr() {
+	a := 100
+	b := 1000
+	ptrSwap(&a, &b)
+	fmt.Println(a, b, 666666666666)
+	fmt.Println(*&a) // 先获取a地址，再获取a地址的值
+}
+
 func max(n1 int, n2 int) int {
 	var res int
 	if n1 > n2 {
@@ -18,6 +32,7 @@ func max(n1 int, n2 int) int {
 
 }
 func main() {
+	ptr()
 	v := max(100, 105)
 	_, next := swap("a", "b")
 	var ptr *string
