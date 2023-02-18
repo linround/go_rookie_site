8c5be3339c0cf59ce48e0649803d546447c1af41
linyuan <673609957@qq.com>
2023-02-18 19:30:26 +0800

    doc: 256

--- src/demo60.go
+++ src/demo60.go
@@ -1,14 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"./person"
+)
 
 type TwoInt struct {
 	a, b int
 }
 
+// add 接收一个指向TwoInt的指针
+
+// 去掉*号就是 add通过拷贝接受TwoInt的值
+
 func (tn *TwoInt) add() int {
+	tn.a = 1000
 	return tn.b + tn.a
 }
+
 func (tn *TwoInt) addParam(p int) int {
 	return tn.a + tn.b + p
 }
@@ -17,4 +27,8 @@ func main() {
 	two := TwoInt{1, 5}
 	fmt.Println(two.add())
 	fmt.Println(two.addParam(4))
+	fmt.Println(two.a)
+
+	p := new(person.Person)
+	p.FirstName()
 }
