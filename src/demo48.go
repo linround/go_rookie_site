e97817b1bad6273e92963691f760f086a419e8c2
linyuan <673609957@qq.com>
2023-02-18 12:35:59 +0800

    doc: 174

---
+++ src/demo48.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s [2]int // 定义切片
+	s[0] = 100
+	x := s // 值传递
+	x[0] = 10000
+	fmt.Print(s)
+	fmt.Print(x)
+	println()
+	sl()
+}
+
+func sl() {
+	// 切片自身就是指针
+	var arr1 [6]int // 定义一个容量为6的切片
+	var slice1 []int = arr1[2:5]
+	for i := 0; i < len(arr1); i++ {
+		arr1[i] = i
+	}
+	fmt.Print(arr1)
+	println()
+	fmt.Print(slice1)
+	fmt.Print(cap(slice1))
+	slice1 = slice1[0:4]
+	println()
+	fmt.Print(slice1)
+
+	// 以下可以证明slice1就是一个指向数组arr1的指针
+	slice1[0] = 45
+	fmt.Print(arr1)
+
+}
