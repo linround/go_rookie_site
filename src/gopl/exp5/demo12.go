766fa70d5d3670eaa2ebae7a67a42f8e1c0a51e9
linyuan <673609957@qq.com>
2023-03-14 15:58:04 +0800

    doc:函数和方法详解

--- src/gopl/exp5/demo12.go
+++ src/gopl/exp5/demo12.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 // 传统的方式通过函数来计算两个点之间的距离 这个是包级别下的函数
 
@@ -15,5 +18,29 @@ type point struct {
 // p 叫做方法接收器
 // 这里定义的是point结构体下声明的方法
 func (p point) distance(q point) float64 {
+
 	return math.Hypot(q.x-p.x, q.y-p.y)
+}
+
+type path []point
+
+func (pa path) distance() float64 {
+	sum := 0.0
+	for i := range pa {
+		if i > 0 {
+			// 计算两个点之间的距离
+			sum += pa[i-1].distance(pa[i])
+		}
+	}
+	return sum
+}
+
+func main() {
+	perim := path{
+		{1, 1},
+		{1, 2},
+		{2, 2},
+		{2, 1},
+	}
+	fmt.Println(perim.distance())
 }
