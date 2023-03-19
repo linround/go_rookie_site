9bd8c5522218ba64ed2e99bfffa7321d28230c8f
linyuan <673609957@qq.com>
2023-03-19 17:54:07 +0800

    doc:底层编程

--- src/reflect/bmain.go
+++ src/reflect/bmain.go
@@ -1,13 +1,19 @@
+// Bzipper reads input, bzip2-compresses it, and writes it out.
 package main
 
 import (
-	"gopl.io/ch13/bzip"
 	"io"
+	"log"
 	"os"
+	"rookie/src/reflect/bzip"
 )
 
 func main() {
 	w := bzip.NewWriter(os.Stdout)
-	io.Copy(w, os.Stdin)
-	w.Close()
+	if _, err := io.Copy(w, os.Stdin); err != nil {
+		log.Fatalf("bzipper: %v\n", err)
+	}
+	if err := w.Close(); err != nil {
+		log.Fatalf("bzipper: close: %v\n", err)
+	}
 }
